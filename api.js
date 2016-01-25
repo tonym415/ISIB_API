818189a5531693425c09ea1d85250583aba72454
gitadmin <antonio.moses79@gmail.com>
2016-01-25 04:23:53 +0000

    Initial JSdoc output

---
+++ api.js
@@ -0,0 +1,37 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "app/appLib.contentMode",
+        "app/appLib.defaults",
+        "app/appLib.steps",
+        "app/appLib.stepsOrientation",
+        "app/appLib.transitionEffect"
+    ],
+    "modules": [
+        "app",
+        "feedback",
+        "game",
+        "signup"
+    ],
+    "allModules": [
+        {
+            "displayName": "app",
+            "name": "app"
+        },
+        {
+            "displayName": "feedback",
+            "name": "feedback",
+            "description": "Handles js interaction for the feedback page"
+        },
+        {
+            "displayName": "game",
+            "name": "game"
+        },
+        {
+            "displayName": "signup",
+            "name": "signup"
+        }
+    ],
+    "elements": []
+} };
+});
